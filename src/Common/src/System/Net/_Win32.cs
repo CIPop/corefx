fc75b8d36f6b49cf1c46a4760826409d001c4c08
Pat Gavlin <pagavlin@microsoft.com>
2015-09-22 14:05:15 -0700

    First round of cleanup in Common.

--- src/Common/src/System/Net/_Win32.cs
+++ src/Common/src/System/Net/_Win32.cs
@@ -6,10 +6,10 @@ namespace System.Net
     internal static class Win32
     {
         internal const int OverlappedInternalOffset = 0;
-        internal static int OverlappedInternalHighOffset = IntPtr.Size;
-        internal static int OverlappedOffsetOffset = IntPtr.Size * 2;
-        internal static int OverlappedOffsetHighOffset = IntPtr.Size * 2 + 4;
-        internal static int OverlappedhEventOffset = IntPtr.Size * 2 + 8;
-        internal static int OverlappedSize = IntPtr.Size * 3 + 8;
+        internal readonly static int OverlappedInternalHighOffset = IntPtr.Size;
+        internal readonly static int OverlappedOffsetOffset = IntPtr.Size * 2;
+        internal readonly static int OverlappedOffsetHighOffset = IntPtr.Size * 2 + 4;
+        internal readonly static int OverlappedhEventOffset = IntPtr.Size * 2 + 8;
+        internal readonly static int OverlappedSize = IntPtr.Size * 3 + 8;
     }
 }
