ac70083d6cbb9964b4ae582eb2a764efbecc4fff
Cristian Pop <Cristian.Pop@microsoft.com>
2017-06-22 16:54:41 -0700

    Disabling test - ILC requires rd.xml hints for reflection to work.

--- src/System.Net.Ping/tests/FunctionalTests/LoggingTest.cs
+++ src/System.Net.Ping/tests/FunctionalTests/LoggingTest.cs
@@ -11,6 +11,7 @@ namespace System.Net.NetworkInformation.Tests
     {
         [Fact]
         [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "NetEventSource is only part of .NET Core")]
+        [ActiveIssue(20470, TargetFrameworkMonikers.UapAot)]
         public void EventSource_ExistsWithCorrectId()
         {
             Type esType = typeof(Ping).Assembly.GetType("System.Net.NetEventSource", throwOnError: true, ignoreCase: false);
