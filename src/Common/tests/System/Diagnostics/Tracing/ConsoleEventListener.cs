caed4652008bf9c351377c75df25ec6691ae3738
Cristian Pop <crispop@microsoft.com>
2018-04-05 13:41:38 -0700

    Enhancement for test ConsoleListener

--- src/Common/tests/System/Diagnostics/Tracing/ConsoleEventListener.cs
+++ src/Common/tests/System/Diagnostics/Tracing/ConsoleEventListener.cs
@@ -2,43 +2,78 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-
 namespace System.Diagnostics.Tracing
 {
-    internal sealed class ConsoleEventListener : EventListener
+    public sealed class ConsoleEventListener : EventListener
     {
-        private readonly string _eventFilter;
+        private readonly string [] _eventFilters;
+        private object _lock = new object();
 
         public ConsoleEventListener() : this(string.Empty) { }
 
         public ConsoleEventListener(string filter)
         {
-            if (filter == null)
-                throw new ArgumentNullException(nameof(filter));
+            _eventFilters = new string[1];
 
-            _eventFilter = filter;
+            _eventFilters[0] = filter ?? throw new ArgumentNullException(nameof(filter));
 
             foreach (EventSource source in EventSource.GetSources())
                 EnableEvents(source, EventLevel.LogAlways);
         }
 
+        public ConsoleEventListener(string [] filters)
+        {
+            if (filters.Length == 0) throw new ArgumentException("Filters cannot be empty");
+            foreach (string filter in filters)
+            {
+                if (string.IsNullOrWhiteSpace(filter))
+                {
+                    throw new ArgumentNullException(nameof(filters));
+                }
+            }
+
+            _eventFilters = filters;
+        }
+
         protected override void OnEventSourceCreated(EventSource eventSource)
         {
             base.OnEventSourceCreated(eventSource);
+#if NET451
+            EnableEvents(eventSource, EventLevel.LogAlways);
+#else
             EnableEvents(eventSource, EventLevel.LogAlways, EventKeywords.All);
+#endif
         }
 
         protected override void OnEventWritten(EventWrittenEventArgs eventData)
         {
-            lock (Console.Out)
+            lock (_lock)
             {
                 string text = $"[{eventData.EventSource.Name}-{eventData.EventId}]{(eventData.Payload != null ? $" ({string.Join(", ", eventData.Payload)})." : "")}";
-                if (_eventFilter != null && text.Contains(_eventFilter))
+
+                bool shouldDisplay = false;
+
+                if (_eventFilters?.Length == 1 && text.Contains(_eventFilters[0]))
+                {
+                    shouldDisplay = true;
+                }
+                else
+                {
+                    foreach (string filter in _eventFilters)
+                    {
+                        if (_eventFilters != null && text.Contains(filter))
+                        {
+                            shouldDisplay = true;
+                        }
+                    }
+                }
+
+                if (shouldDisplay)
                 {
                     ConsoleColor origForeground = Console.ForegroundColor;
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine(text);
+                    Debug.WriteLine(text);
                     Console.ForegroundColor = origForeground;
                 }
             }
